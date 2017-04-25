79c3c285ce55bdaa6b0e25de94111f8e29b3c909
Adam Hansen <adamlars90@gmail.com>
2017-04-25 13:34:40 +0200

    Restructured project

--- src/museumtimetracking/gui/views/root/guild/newGuildView/controller/NewGuildViewController.java
+++ src/museumtimetracking/gui/views/root/guild/newGuildView/controller/NewGuildViewController.java
@@ -3,26 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package museumtimetracking.gui.main.controller;
+package museumtimetracking.gui.views.root.guild.newGuildView.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.BorderPane;
 
 /**
+ * FXML Controller class
  *
- * @author gta1
+ * @author Skovgaard
  */
-public class MTTMainController implements Initializable {
+public class NewGuildViewController implements Initializable {
 
-    @FXML
-    private BorderPane borderPainMain;
-
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }
-
+    }    
+    
 }
