9f6b4e86061ade2ba54dbea4a21937764809522b
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-04-26 12:15:25 +0200

    Made the C in CRUD for guildmanager.

--- src/museumtimetracking/be/GuildManager.java
+++ src/museumtimetracking/be/GuildManager.java
@@ -16,6 +16,11 @@ public class GuildManager extends APerson {
 
     private final List<Guild> listOfGuilds;
 
+    public GuildManager(String firstName, String lastName, String email, int phone) {
+        super(firstName, lastName, email, phone, 0);
+        listOfGuilds = new ArrayList();
+    }
+
     public GuildManager(String firstName, String lastName, String email, int phone, int ID) {
         super(firstName, lastName, email, phone, ID);
         listOfGuilds = new ArrayList();
