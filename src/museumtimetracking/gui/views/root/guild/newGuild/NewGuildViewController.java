d899fd227925333badebad56470ef6f3e0dce845
Sk0vgaard <Sk0vgaard@live.dk>
2017-04-26 12:54:19 +0200

    Can now save new guilds with descriptions in database.

--- src/museumtimetracking/gui/views/root/guild/newGuild/NewGuildViewController.java
+++ src/museumtimetracking/gui/views/root/guild/newGuild/NewGuildViewController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import museumtimetracking.be.Guild;
+import museumtimetracking.gui.model.GuildModel;
 
 /**
  * FXML Controller class
@@ -29,16 +31,26 @@ public class NewGuildViewController implements Initializable {
     private TextArea txtAreaGuildDescription;
     @FXML
     private TextField txtFieldGuildName;
+    
+    private GuildModel guildModel;
 
+    public NewGuildViewController() {
+        guildModel = new GuildModel();
+    }
+    
+    
     @FXML
     private void handleAddGuildBtn(ActionEvent event) {
+        Guild newGuild = new Guild(txtFieldGuildName.getText(), txtAreaGuildDescription.getText());
+        guildModel.addGuild(newGuild);
+        
+        closeWindow();
     }
 
     @FXML
     // How to close a window.
     private void handleBackBtn(ActionEvent event) {
-        Stage stage = (Stage) txtFieldGuildName.getScene().getWindow();
-        stage.close();
+        closeWindow();
     }
 
     /**
@@ -47,6 +59,11 @@ public class NewGuildViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+    
+    private void closeWindow(){
+        Stage stage = (Stage) txtFieldGuildName.getScene().getWindow();
+        stage.close();
     }
 
 }
