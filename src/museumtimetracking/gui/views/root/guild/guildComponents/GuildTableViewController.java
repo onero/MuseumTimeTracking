47eaaaae550f26a4ee448ee21d4b92b6930ed7aa
Sk0vgaard <Sk0vgaard@live.dk>
2017-04-27 12:06:37 +0200

    Alertbox
    Alertbox for deleting guild

--- src/museumtimetracking/gui/views/root/guild/guildComponents/GuildTableViewController.java
+++ src/museumtimetracking/gui/views/root/guild/guildComponents/GuildTableViewController.java
@@ -14,6 +14,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
@@ -60,16 +63,27 @@ public class GuildTableViewController implements Initializable {
     }
 
     /**
-     * Deletes the selected guild(s) from tableView and DB.
-     * Goes to GuildModel.
+     * Deletes the selected guild(s) from tableView and DB. Goes to GuildModel.
      */
     @FXML
-
     private void handleDeleteGuild() {
-        Guild deleteGuild = tableGuild.getSelectionModel().getSelectedItem();
-        guildModel.deleteGuild(deleteGuild);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setHeaderText(" Tryk 'Ja' for at slette permanent. \n Tryk 'Nej' for at fortryde.");
+        ButtonType yesButton = new ButtonType("Ja", ButtonBar.ButtonData.YES);
+        ButtonType noButton = new ButtonType("Nej", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(yesButton, noButton);
+        alert.showAndWait().ifPresent(type -> {
+
+            if (type == yesButton) {
+                Guild deleteGuild = tableGuild.getSelectionModel().getSelectedItem();
+                guildModel.deleteGuild(deleteGuild);
+
+            }
+        });
+
     }
-        
+
     @FXML
     private void handleArchiveBtn() {
         Guild guildToArchive = tableGuild.getSelectionModel().getSelectedItem();
@@ -97,7 +111,6 @@ public class GuildTableViewController implements Initializable {
 
             newStage.show();
         }
-
     }
 
     /**
