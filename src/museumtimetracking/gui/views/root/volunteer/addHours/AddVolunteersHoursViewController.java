e12139c393ed4b1d3c87db6be08d8de1d1d46cc1
LindvedKrvang <rklindved@gmail.com>
2017-05-03 13:39:43 +0200

    Iitialized spinner

---
+++ src/museumtimetracking/gui/views/root/volunteer/addHours/AddVolunteersHoursViewController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museumtimetracking.gui.views.root.volunteer.addHours;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
+import museumtimetracking.be.Guild;
+import museumtimetracking.be.Volunteer;
+import museumtimetracking.gui.model.GuildModel;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rasmus
+ */
+public class AddVolunteersHoursViewController implements Initializable {
+
+    @FXML
+    private ListView<Guild> lstGuilds;
+    @FXML
+    private Label lblGuildName;
+    @FXML
+    private Spinner<Integer> spnHours;
+
+    private final int MINNIMUM_RANGE = 0;
+    private final int MAXIMUM_RANGE = 20;
+    private final int INITIAL_VALUE = 8;
+
+    private final GuildModel guildmodel;
+    private Volunteer volunteer;
+
+    public AddVolunteersHoursViewController() {
+        guildmodel = GuildModel.getInstance();
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setCellFactory();
+        lstGuilds.setItems(guildmodel.getCachedGuilds());
+        addListeners();
+        initializeSpinner();
+    }
+
+    @FXML
+    private void handleDocumenButton(ActionEvent event) {
+        closeModal();
+    }
+
+    @FXML
+    private void handleCancelButton(ActionEvent event) {
+        closeModal();
+    }
+
+    /**
+     * Adds a changeListener for lstGuilds.
+     */
+    private void addListeners() {
+        lstGuilds.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Guild>() {
+            @Override
+            public void changed(ObservableValue<? extends Guild> observable, Guild oldValue, Guild newValue) {
+                if (newValue != oldValue) {
+                    lblGuildName.setText(newValue.getName());
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets the cellFactory of the lstGuilds.
+     */
+    private void setCellFactory() {
+        lstGuilds.setCellFactory(g -> new ListCell<Guild>() {
+            @Override
+            protected void updateItem(Guild item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets the volunteer.
+     *
+     * @param volunteer
+     */
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    /**
+     * Closes the modal.
+     */
+    private void closeModal() {
+        Stage stage = (Stage) lblGuildName.getScene().getWindow();
+        stage.close();
+    }
+
+    private void initializeSpinner() {
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory
+                = new SpinnerValueFactory.IntegerSpinnerValueFactory(
+                        MINNIMUM_RANGE,
+                        MAXIMUM_RANGE,
+                        INITIAL_VALUE);
+        spnHours.setValueFactory(valueFactory);
+    }
+
+}
