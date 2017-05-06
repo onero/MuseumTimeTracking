c23ad9c24a9cd2ceee46ca56fc42d09754caa623
Adam Hansen <adamlars90@gmail.com>
2017-05-06 18:12:36 +0200

    Started on some really crazy things!!!

--- src/museumtimetracking/gui/views/root/MTTMainControllerView.java
+++ src/museumtimetracking/gui/views/root/MTTMainControllerView.java
@@ -5,92 +5,198 @@
  */
 package museumtimetracking.gui.views.root;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import static museumtimetracking.be.enums.EFXMLName.*;
-import museumtimetracking.gui.views.NodeFactory;
-import museumtimetracking.gui.views.root.guild.guildSearch.GuildSearchViewController;
-import museumtimetracking.gui.views.root.guildManager.guildManagerSearch.GuildManagerSearchController;
-import museumtimetracking.gui.views.root.volunteer.volunteerSearch.VolunteerSearchController;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import museumtimetracking.be.Guild;
+import museumtimetracking.be.enums.EFXMLName;
+import museumtimetracking.exception.AlertFactory;
+import museumtimetracking.exception.DALException;
+import museumtimetracking.exception.ExceptionDisplayer;
+import museumtimetracking.gui.model.GuildModel;
+import museumtimetracking.gui.views.ModalFactory;
+import museumtimetracking.gui.views.root.guild.editGuild.EditGuildViewController;
 
 /**
+ * FXML Controller class
  *
  * @author gta1
  */
 public class MTTMainControllerView implements Initializable {
 
     @FXML
-    private BorderPane borderPaneMain;
+    private AnchorPane anchorPaneGuild;
+
     @FXML
-    private BorderPane borderPaneMainAtBottomBorderPane;
+    private BorderPane borderPane;
+    @FXML
+    private BarChart<?, ?> chartHoursOverview;
+    @FXML
+    private TableColumn<Guild, String> clmGuildName;
+    @FXML
+    private TableColumn<Guild, String> clmbGuildDescription;
+    @FXML
+    private JFXComboBox<?> cmbGuildManager;
+    @FXML
+    private GridPane gridPaneGuild;
+    @FXML
+    private Tab tabGM;
+    @FXML
+    private JFXTabPane tabPane;
+    @FXML
+    private Tab tabPaneGuild;
+    @FXML
+    private Tab tabVolunteer;
+    @FXML
+    private TableView<Guild> tblGuild;
+    @FXML
+    private JFXTextArea txtDescription;
+    @FXML
+    private JFXTextField txtGuildName;
+    @FXML
+    private VBox vBoxGuildOptions;
 
-    private final NodeFactory nodeFactory;
+    private GuildModel guildModel;
 
-    private final Node guildOverview;
-    private final Node managerOverview;
-    private final Node volunteerOverview;
-    private final Node archivedGuilds;
-    private final Node idleVolunteer;
-    private final Node idleGuildManager;
-    private final Node guildSearch;
-    private final Node guildManagerSearch;
-    private final Node volunteerSearch;
+    private ModalFactory modalFactory;
 
-    private final GuildSearchViewController guildSearchViewController;
-    private final GuildManagerSearchController guildManagerSearchController;
-    private final VolunteerSearchController volunteerSearchController;
+    private Guild selectedGuild;
 
     public MTTMainControllerView() {
-        nodeFactory = NodeFactory.getInstance();
-
-        guildOverview = nodeFactory.createNewView(GUILD_OVERVIEW);
-        managerOverview = nodeFactory.createNewView(MANAGER_OVERVIEW);
-        volunteerOverview = nodeFactory.createNewView(VOLUNTEER_OVERVIEW);
-        archivedGuilds = nodeFactory.createNewView(ARCHIVED_TABLE);
-        idleVolunteer = nodeFactory.createNewView(IDLE_VOLUNTEER);
-        idleGuildManager = nodeFactory.createNewView(IDLE_MANAGER);
-
-        //Initializes the SearchViews.
-        guildSearch = nodeFactory.createNewView(GUILD_SEARCH);
-        guildSearchViewController = nodeFactory.getLoader().getController();
-        guildManagerSearch = nodeFactory.createNewView(GUILD_MANAGER_SEARCH);
-        guildManagerSearchController = nodeFactory.getLoader().getController();
-        volunteerSearch = nodeFactory.createNewView(VOLUNTEER_SEARCH);
-        volunteerSearchController = nodeFactory.getLoader().getController();
+        modalFactory = ModalFactory.getInstance();
+        try {
+            this.guildModel = GuildModel.getInstance();
+        } catch (IOException | DALException ex) {
+            ExceptionDisplayer.display(ex);
+        }
     }
 
+    @FXML
+    private void handleEditGuild(MouseEvent event) {
+        setGuildOptionsVisibility(true);
+
+        selectedGuild = tblGuild.getSelectionModel().getSelectedItem();
+
+        giveDataToChartHoursOverview(selectedGuild.getName());
+
+        //Enter edit mode
+        if (event.getClickCount() == 2) {
+
+            Stage primStage = (Stage) tblGuild.getScene().getWindow();
+
+            Stage editGuildModal = modalFactory.createNewModal(primStage, EFXMLName.EDIT_GUILD);
+
+            EditGuildViewController controller = modalFactory.getLoader().getController();
+
+            controller.setCurrentGuild(selectedGuild);
+
+            editGuildModal.show();
+        }
+    }
+
+    /**
+     * Set the visibility of guild options
+     *
+     * @param shown
+     */
+    public void setGuildOptionsVisibility(boolean shown) {
+        vBoxGuildOptions.setDisable(!shown);
+        vBoxGuildOptions.setVisible(shown);
+    }
+
+    @FXML
+    private void handleAddGuild() {
+        String name = txtGuildName.getText();
+        String description = txtDescription.getText();
+        if (name != null && description != null) {
+            Guild newGuild = new Guild(name, description, false);
+            try {
+                guildModel.addGuild(newGuild);
+            } catch (DALException ex) {
+                ExceptionDisplayer.display(ex);
+            }
+        }
+    }
+
+    @FXML
+    private void handleArchiveGuid() {
+        try {
+            guildModel.archiveGuild(selectedGuild);
+        } catch (DALException ex) {
+            ExceptionDisplayer.display(ex);
+        }
+    }
+
+    @FXML
+    private void handleDeleteGuid() {
+        Alert alert = AlertFactory.createDeleteAlert();
+        alert.showAndWait().ifPresent(type -> {
+            //If the first button ("YES") is clicked
+            if (type == alert.getButtonTypes().get(0)) {
+                try {
+                    guildModel.deleteGuild(selectedGuild);
+                } catch (DALException ex) {
+                    ExceptionDisplayer.display(ex);
+                }
+            }
+        });
+
+    }
+
+    @FXML
+    private void handleSelectGuildManager(ActionEvent event) {
+    }
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        borderPaneMain.setCenter(guildOverview);
-        borderPaneMain.setTop(guildSearch);
-        borderPaneMainAtBottomBorderPane.setCenter(archivedGuilds);
+        tblGuild.setItems(guildModel.getCachedGuilds());
+
+        clmGuildName.setCellValueFactory(g -> g.getValue().getNameProperty());
+        clmbGuildDescription.setCellValueFactory(g -> g.getValue().getDescriptionProperty());
+
     }
 
-    @FXML
-    private void handleGuildsButton() {
-        borderPaneMain.setCenter(guildOverview);
-        borderPaneMain.setTop(guildSearch);
-        guildSearchViewController.clearSearchBar();
-        borderPaneMainAtBottomBorderPane.setCenter(archivedGuilds);
+    /**
+     *
+     * @param title
+     */
+    private void giveDataToChartHoursOverview(String title) {
+        chartHoursOverview.getData().clear();
+        chartHoursOverview.setTitle(title);
+        List<Guild> guilds = guildModel.getGuildsFromDB();
+        Map<String, Integer> guildHours = guildModel.getMapOfHoursPerGuild();
+
+        XYChart.Series hoursSeries = new XYChart.Series<>();
+
+        for (Guild guild : guilds) {
+            hoursSeries.getData().add(new XYChart.Data<>(guild.getName(), guildHours.get(guild.getName())));
+        }
+
+        chartHoursOverview.getData().add(hoursSeries);
     }
 
-    @FXML
-    private void handleGuildManagersButton() {
-        borderPaneMain.setCenter(managerOverview);
-        borderPaneMainAtBottomBorderPane.setCenter(idleGuildManager);
-        borderPaneMain.setTop(guildManagerSearch);
-        guildManagerSearchController.clearSearchBar();
-    }
-
-    @FXML
-    private void handleVolunteersButton() {
-        borderPaneMain.setCenter(volunteerOverview);
-        borderPaneMain.setTop(volunteerSearch);
-        volunteerSearchController.clearSearchBar();
-        borderPaneMainAtBottomBorderPane.setCenter(idleVolunteer);
-    }
 }
--- src/museumtimetracking/gui/views/root/guild/GuildOverviewController.java
+++ src/museumtimetracking/gui/views/root/guild/GuildOverviewController.java
@@ -5,23 +5,29 @@
  */
 package museumtimetracking.gui.views.root.guild;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import museumtimetracking.be.Guild;
+import museumtimetracking.be.GuildManager;
 import museumtimetracking.be.enums.EFXMLName;
 import museumtimetracking.exception.AlertFactory;
 import museumtimetracking.exception.DALException;
@@ -38,21 +44,31 @@ import museumtimetracking.gui.views.root.guild.editGuild.EditGuildViewController
 public class GuildOverviewController implements Initializable {
 
     @FXML
-    private TableColumn<Guild, String> clmGuildDescription;
+    private TableView<Guild> tblGuild;
     @FXML
     private TableColumn<Guild, String> clmGuildName;
     @FXML
+    private TableColumn<Guild, String> clmGuildDescription;
+    @FXML
+    private JFXComboBox<GuildManager> cmbGuildManager;
+    @FXML
+    private GridPane gridPaneGuild;
+    @FXML
     private BorderPane guildBorderPane;
     @FXML
-    private TableView<Guild> tableGuild;
-    @FXML
     private BarChart<String, Integer> chartHoursOverview;
-
-    private final ModalFactory modalFactory;
+    @FXML
+    private JFXTextArea txtDescription;
+    @FXML
+    private JFXTextField txtGuildName;
+    @FXML
+    private VBox vBoxGuildOptions;
 
     private GuildModel guildModel;
 
-    private Stage primStage;
+    private ModalFactory modalFactory;
+
+    private Guild selectedGuild;
 
     public GuildOverviewController() {
         modalFactory = ModalFactory.getInstance();
@@ -63,58 +79,18 @@ public class GuildOverviewController implements Initializable {
         }
     }
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        tableGuild.setItems(guildModel.getCachedGuilds());
-
-        clmGuildName.setCellValueFactory(g -> g.getValue().getNameProperty());
-        clmGuildDescription.setCellValueFactory(g -> g.getValue().getDescriptionProperty());
-        initializeChartHoursOverview();
-    }
-
-    /**
-     * Deletes the selected guild from tableView and DB. Goes to GuildModel.
-     */
     @FXML
-    private void handleDeleteGuild() {
-        String message = "Tryk 'Ja' for at slette permanent. \n Tryk 'Nej' for at fortryde.";
-        Alert alert = AlertFactory.createAlert(AlertType.WARNING, message);
-        alert.showAndWait().ifPresent(type -> {
-            //If the first button ("YES") is clicked
-            if (type == alert.getButtonTypes().get(0)) {
-                Guild deleteGuild = tableGuild.getSelectionModel().getSelectedItem();
-                try {
-                    guildModel.deleteGuild(deleteGuild);
-                } catch (DALException ex) {
-                    ExceptionDisplayer.display(ex);
-                }
-            }
-        });
+    private void handleEditGuild(MouseEvent event) {
+        setGuildOptionsVisibility(true);
 
-    }
+        selectedGuild = tblGuild.getSelectionModel().getSelectedItem();
 
-    @FXML
-    private void handleArchiveBtn() {
-        Guild guildToArchive = tableGuild.getSelectionModel().getSelectedItem();
-        if (guildToArchive != null) {
-            try {
-                guildModel.archiveGuild(guildToArchive);
-            } catch (DALException ex) {
-                ExceptionDisplayer.display(ex);
-            }
-        }
-    }
+        giveDataToChartHoursOverview(selectedGuild.getName());
 
-    @FXML
-    private void handleEditGuild(MouseEvent event) throws IOException {
-
+        //Enter edit mode
         if (event.getClickCount() == 2) {
-            Guild selectedGuild = tableGuild.getSelectionModel().getSelectedItem();
 
-            primStage = (Stage) tableGuild.getScene().getWindow();
+            Stage primStage = (Stage) tblGuild.getScene().getWindow();
 
             Stage editGuildModal = modalFactory.createNewModal(primStage, EFXMLName.EDIT_GUILD);
 
@@ -126,22 +102,69 @@ public class GuildOverviewController implements Initializable {
         }
     }
 
+    /**
+     * Set the visibility of guild options
+     *
+     * @param shown
+     */
+    public void setGuildOptionsVisibility(boolean shown) {
+        vBoxGuildOptions.setDisable(!shown);
+        vBoxGuildOptions.setVisible(shown);
+    }
+
     @FXML
-    private void handleAddGuildBtn() throws IOException {
-        primStage = (Stage) tableGuild.getScene().getWindow();
+    private void handleAddGuild() {
+        String name = txtGuildName.getText();
+        String description = txtDescription.getText();
+        if (name != null && description != null) {
+            Guild newGuild = new Guild(name, description, false);
+            try {
+                guildModel.addGuild(newGuild);
+            } catch (DALException ex) {
+                ExceptionDisplayer.display(ex);
+            }
+        }
+    }
 
-        Stage editGuildModal = modalFactory.createNewModal(primStage, EFXMLName.ADD_NEW_GUILD);
+    @FXML
+    private void handleArchiveGuid() {
+        try {
+            guildModel.archiveGuild(selectedGuild);
+        } catch (DALException ex) {
+            ExceptionDisplayer.display(ex);
+        }
+    }
 
-        editGuildModal.show();
+    @FXML
+    private void handleDeleteGuid() {
+        Alert alert = AlertFactory.createDeleteAlert();
+        alert.showAndWait().ifPresent(type -> {
+            //If the first button ("YES") is clicked
+            if (type == alert.getButtonTypes().get(0)) {
+                try {
+                    guildModel.deleteGuild(selectedGuild);
+                } catch (DALException ex) {
+                    ExceptionDisplayer.display(ex);
+                }
+            }
+        });
 
     }
 
+    @FXML
+    private void handleSelectGuildManager(ActionEvent event) {
+    }
+
     /**
-     * Sets the title of the chart and give it it's initial data.
+     * Initializes the controller class.
      */
-    private void initializeChartHoursOverview() {
-        chartHoursOverview.setTitle("Dokumenterede Timer");
-        giveDataToChartHoursOverview("Laug");
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        tblGuild.setItems(guildModel.getCachedGuilds());
+
+        clmGuildName.setCellValueFactory(g -> g.getValue().getNameProperty());
+        clmGuildDescription.setCellValueFactory(g -> g.getValue().getDescriptionProperty());
+
     }
 
     /**
@@ -150,11 +173,11 @@ public class GuildOverviewController implements Initializable {
      */
     private void giveDataToChartHoursOverview(String title) {
         chartHoursOverview.getData().clear();
+        chartHoursOverview.setTitle(title);
         List<Guild> guilds = guildModel.getGuildsFromDB();
         Map<String, Integer> guildHours = guildModel.getMapOfHoursPerGuild();
 
         XYChart.Series hoursSeries = new XYChart.Series<>();
-        hoursSeries.setName(title);
 
         for (Guild guild : guilds) {
             hoursSeries.getData().add(new XYChart.Data<>(guild.getName(), guildHours.get(guild.getName())));
