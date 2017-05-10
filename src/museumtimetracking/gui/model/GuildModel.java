47ef6f244f58182f1deef0fe660f170810491bf1
Adam Hansen <adamlars90@gmail.com>
2017-05-10 22:23:30 +0200

    Fixed critical NullPointer

--- src/museumtimetracking/gui/model/GuildModel.java
+++ src/museumtimetracking/gui/model/GuildModel.java
@@ -176,7 +176,8 @@ public class GuildModel {
     public void searchGuilds(String newValue) {
         cachedGuilds.clear();
         guildsFromDB.stream()
-                .filter(g -> g.getName().toLowerCase().contains(newValue.toLowerCase()) || g.getGuildManager().getFullName().toLowerCase().contains(newValue.toLowerCase()))
+                .filter(g -> g.getName().toLowerCase().contains(newValue.toLowerCase())
+                || g.getGuildManager() != null && g.getGuildManager().getFullName().toLowerCase().contains(newValue.toLowerCase()))
                 .forEach(g -> cachedGuilds.add(g));
     }
 
