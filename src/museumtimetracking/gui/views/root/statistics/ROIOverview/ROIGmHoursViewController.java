d475fe504295adf55a7b96613ca1fe91f592960a
LindvedKrvang <rklindved@gmail.com>
2017-05-15 13:01:58 +0200

    PieChart now shows data
    OBS: Refactoring still needed

--- src/museumtimetracking/gui/views/root/statistics/ROIOverview/ROIGmHoursViewController.java
+++ src/museumtimetracking/gui/views/root/statistics/ROIOverview/ROIGmHoursViewController.java
@@ -7,10 +7,16 @@ package museumtimetracking.gui.views.root.statistics.ROIOverview;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
+import museumtimetracking.be.Guild;
 import museumtimetracking.exception.DALException;
 import museumtimetracking.exception.ExceptionDisplayer;
 import museumtimetracking.gui.model.GuildModel;
@@ -41,10 +47,28 @@ public class ROIGmHoursViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         chartPie.setLegendVisible(false);
+        updateDataForChart();
     }
 
+    /**
+     * Updates the pieChart with ROI data.
+     */
     public void updateDataForChart() {
+        List<Guild> guilds = guildModel.getGuildsFromDB();
+        Map<String, Integer> ROIHours = null;
+        try {
+            ROIHours = guildModel.getGMROIOnVolunteerForAMonth(guilds, 2);
+        } catch (DALException ex) {
+            ExceptionDisplayer.display(ex);
+        }
 
+        ObservableList<Data> chartData = FXCollections.observableArrayList();
+        if (ROIHours != null) {
+            for (Map.Entry<String, Integer> entry : ROIHours.entrySet()) {
+                chartData.add(new Data(entry.getKey(), entry.getValue()));
+            }
+            chartPie.setData(chartData);
+        }
     }
 
 }
