875d5e2582fd4fc89d7d600677d9384cb1ab3099
Sk0vgaard <Sk0vgaard@live.dk>
2017-05-23 11:20:04 +0200

    Fixed conflict

--- src/museumtimetracking/gui/views/root/idle/IdleViewController.java
+++ src/museumtimetracking/gui/views/root/idle/IdleViewController.java
@@ -157,13 +157,13 @@ public class IdleViewController implements Initializable {
 
     @FXML
     private void handleSelectGM() {
-//        selectedManager = tableIdleGM.getSelectionModel().getSelectedItem();
+        selectedManager = tableIdleGM.getSelectionModel().getSelectedItem();
     }
 
     @FXML
     private void handleSelectVounteer() {
         setIdleVolunteerOptionsVisibility(true);
-//        selectedVolunteer = tableIdleVolunteer.getSelectionModel().getSelectedItem();
+        selectedVolunteer = tableIdleVolunteer.getSelectionModel().getSelectedItem();
     }
 
     /**
